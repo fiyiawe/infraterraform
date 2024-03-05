3375796323ee68ceb00f0541ea841f67de1933d3
OLUWAFIYIWEMI AWE <81171382+fiyiawe@users.noreply.github.com>
2024-03-05 15:58:57 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "myterraformstatebucket1212"
-    key = "terraform1212.tfstate"
+    bucket = "fiyiterraformstatebucket1212"
+    key = "terraform.tfstate"
     region = "eu-west-2"
-    dynamodb_table = "fiyiterraformstateblock"
+    dynamodb_table = "fiyi_dynamodb_table"
   }
 }
